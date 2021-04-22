391c56279fa77f727cba3880812be27d8ee8247c
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-04-23 00:49:35 +0600

    Update Below the Main Diagonal.cpp

--- URI Judge/Below the Main Diagonal.cpp
+++ URI Judge/Below the Main Diagonal.cpp
@@ -1,1 +1,45 @@
 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    double sum=0.0, M[12][12];
+    char T[2];
+    int i,j;
+
+    scanf("%s", &T);
+
+    for(i=0; i<=11; i++)
+    {
+        for(j=0; j<=11; j++)
+        {
+            scanf("%lf", &M[i][j]);
+
+        }
+    }
+
+    for(i=0; i<=11; i++){
+
+        for(j=0; j<=11; j++){
+           if (i > j) {
+            sum = sum + M[i][j];
+         }
+        }
+
+     }
+
+
+    if(T[0]=='S')
+    {
+        printf("%.1lf\n",sum);
+    }
+    else if(T[0]=='M')
+    {
+        sum=sum/66.0;
+        printf("%.1lf\n",sum);
+    }
+    return 0;
+}
+
