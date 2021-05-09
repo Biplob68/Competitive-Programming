92e24fc07af5713c19a5b7d33db147290fe42fcb
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-05-09 22:50:03 +0600

    Update Linklist Implementation.cpp

--- Practice Code/Linklist Implementation.cpp
+++ Practice Code/Linklist Implementation.cpp
@@ -7,29 +7,44 @@ struct Node
     struct Node *next;
 };
 
+
+// print the linked list value
+void printLinkedlist(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d->", p->data);
+        p = p->next;
+    }
+}
+
+
 int main()
 {
+    struct Node *head;
     struct Node *a = NULL;
     struct Node *b = NULL;
     struct Node *c = NULL;
 
+    // Allocate memory
     a = (struct Node*)malloc(sizeof(struct Node));
     b = (struct Node*)malloc(sizeof(struct Node));
     c = (struct Node*)malloc(sizeof(struct Node));
 
+
+    // Assign value values
     a->data = 10;
     b->data = 20;
     c->data = 30;
 
+    // Connect nodes
     a->next = b;
     b->next = c;
     c->next = NULL;
 
-    while(a!=NULL){
-        printf("%d\n",a->data);
-        a = a->next;
-    }
-
+    // printing node-value
+    head = a;
+    printLinkedlist(head);
 
     return 0;
 }
