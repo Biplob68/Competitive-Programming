12ece14abc235f210c43b92ce8b01a108e9c5911
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-05-21 21:05:15 +0600

    Create BinarySearch.cpp

---
+++ Practice Code/BinarySearch.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int binarysearch(int arra[], int n, int x)
+{
+    int left, right, mid;
+    left = 0;
+    right = n-1;
+
+    while(left <= right)
+    {
+        mid = (left + right)/2;
+
+        if(arra[mid] == x) return mid;
+        if(arra[mid] < x)  left = mid+1;
+        else right = mid-1;
+    }
+    return -1;
+
+}
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int n = sizeof(array) / sizeof(array[0]);
+    int x = 8;
+    int result = binarysearch(array, n, x);
+
+    if (result == -1)
+        printf("Not found\n");
+    else
+        printf("Element is found at index %d\n", result+1);
+
+    return 0;
+}
