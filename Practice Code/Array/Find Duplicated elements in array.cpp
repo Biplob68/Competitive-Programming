600d2577d08095f2c5e18badbf8e641793ca8409
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-06-01 10:59:50 +0600

    Update Find Duplicated elements in array.cpp

--- Practice Code/Array/Find Duplicated elements in array.cpp
+++ Practice Code/Array/Find Duplicated elements in array.cpp
@@ -2,20 +2,23 @@
 
 int main()
 {
-    int i,j,cnt;
-    int arra[10];
+    int i,j,cnt,n;
+
+    printf("Enter the size of araay :");
+    scanf("%d",&n);
+    int arra[n];
 
     printf("Enter the Elements of Array\n");
-    for(i=0; i<5; i++)
+    for(i=0; i<n; i++)
     {
         printf("%d th Element : ",i+1);
         scanf("%d",&arra[i]);
     }
 
     printf("Duplicated elements of array : ");
-    for(i=0; i<5; i++)
+    for(i=0; i<n; i++)
     {
-        for(j=i+1; j<5; j++)
+        for(j=i+1; j<n; j++)
         {
             if(arra[i] == arra[j])
             {
