c08020f07a98a6315a92d0594b531c0c0278442a
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-07-01 18:49:59 +0600

    Create Bubble Sort.cpp

---
+++ Practice Code/Sorting Algorithm/Bubble Sort.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+// Bubble sort algorithm
+void bubble_sort(int arra[], int n)
+{
+    int i,j,temp;
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n-i-1; j++)
+        {
+            if(arra[j] > arra[j+1])
+            {
+                temp = arra[j+1];
+                arra[j+1] = arra[j];
+                arra[j] = temp;
+
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int arra[] = {1,5,6,12,9,10,3};
+    bubble_sort(arra, 7);
+
+    for(int i=0; i<7; i++){
+        printf("%d ", arra[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+
