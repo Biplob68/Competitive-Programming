f8e090c8b9b21d3fdd697a6d5ab588fa739126d1
Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-07-27 12:47:58 +0600

    Update Pair of integer whose sum is equal of a number.c

--- Practice Code/Array/Pair of integer whose sum is equal of a number.c
+++ Practice Code/Array/Pair of integer whose sum is equal of a number.c
@@ -34,3 +34,50 @@ int main()
     return 0;
 
 }
+
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Function to find a pair in an array with a given sum using sorting
+void findPair(int arr[], int n, int sum)
+{
+    // sort the array in ascending order
+    sort(arr, arr + n);
+
+    // maintain two pointing to endpoints of the array
+    int low = 0;
+    int high = n - 1;
+
+    while (low < high)
+    {
+        if (arr[low] + arr[high] == sum)
+        {
+            printf("Pair found %d , %d\n",arr[low], arr[high]);
+            return;
+        }
+        // increment `low` index if the total is less than the desired sum;
+        // decrement `high` index if the total is more than the desired sum
+        if (arr[low] + arr[high] > sum) high--;
+
+        else low++;
+    }
+    // If the pair is not found
+    printf("Pair is not found!\n");
+}
+
+int main()
+{
+    int arr[] = { 8, 7, 2, 5, 3, 1 };
+    int sum =8;
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    findPair(arr, n, sum);
+
+    return 0;
+}
+Complexity -> nlog(n) - for quicksort
+*/
