5e628a76624d2e659574fa1994a8d5598d2f9bf4
Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-08-03 10:27:39 +0600

    Update Binary Search Tree Implementation.cpp

--- Practice Code/Tree/Binary Search Tree/Binary Search Tree Implementation.cpp
+++ Practice Code/Tree/Binary Search Tree/Binary Search Tree Implementation.cpp
@@ -89,3 +89,31 @@ int main()
 }
 
 // Complexity : O(log(n) -> Best, Average case..... O(n) -> Worst case.
+
+/*----------------------------------------------------------
+// Search an item into binary search tree.
+struct node* search(int data){ 
+   struct node *current = root;
+   printf("Visiting elements: ");
+	
+   while(current->data != data){
+	
+      if(current != NULL) {
+         printf("%d ",current->data);
+			
+         //go to left tree
+         if(current->data > data){
+            current = current->leftChild;
+         }//else go to right tree
+         else {                
+            current = current->rightChild;
+         }
+			
+         //not found
+         if(current == NULL){
+            return NULL;
+         }
+      }			
+   }
+   return current;
+}*/
