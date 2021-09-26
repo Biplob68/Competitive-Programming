29963b49e629e066d97e556c95306d2a5987f246
Biplob <66861180+Biplob68@users.noreply.github.com>
2021-09-26 12:24:50 +0600

    Create 191. Number of 1 Bits.cpp

---
+++ Leetcode/191. Number of 1 Bits.cpp
@@ -0,0 +1,32 @@
+/* Problem : Write a function that takes an unsigned integer and returns the number of '1' bits it has
+Input: n = 00000000000000000000000000001011
+Output: 3
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int hammingWeight(uint32_t n) {
+        
+        int cnt = 0;
+        
+        
+        while(n){
+            // Bitwise AND operation 
+            n = n & (n-1);
+            cnt++;
+        }
+        return cnt;
+}
+
+int main()
+{
+  uint32_t num;
+  cin >> num ;
+  
+  cout << hamminWeight(num) << endl;
+  
+  return 0;
+}
+
+// Complexity : O(logn)
