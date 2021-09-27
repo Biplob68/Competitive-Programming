8c97a2ad49659abced923b3f3e31f80a0f43a7ab
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-09-27 14:44:11 +0600

    Create 67. Add Binary.cpp

---
+++ Leetcode/67. Add Binary.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// function to add two binary number -> 101+100 = 1001
+string addBinary(string a, string b)
+{
+    string result = "";
+
+    int sum = 0;
+
+    int len1 = a.length()-1;
+    int len2 = b.length()-1;
+
+    while(len1>=0 || len2>=0 || sum==1)
+    {
+        // find out sum of last digits
+        sum += ( (len1>=0)? a[len1] - '0' : 0);
+        sum += ( (len2>=0)? b[len2] - '0' : 0);
+
+        // If current sum is 1->1 or 3->11 then add 1 to result
+        // If current sum is 2->10 then add 0 to the result
+        result = char(sum%2 - '0') + result;
+
+        // carry of the current sum;
+        sum = sum / 2;
+
+        len1--;
+        len2--;
+    }
+
+    return result;
+}
+
+int main()
+{
+
+    string a, b;
+
+    cin >> a >> b;
+
+    cout << addBinary(a,b) << endl;
+
+    return 0;
+}
