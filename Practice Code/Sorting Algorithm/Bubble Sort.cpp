a465e6640b6e78db67fee1e3f131ece33a11b44d
Biplob <66861180+Biplob68@users.noreply.github.com>
2022-02-07 15:57:29 +0600

    Update Bubble Sort.cpp

--- Practice Code/Sorting Algorithm/Bubble Sort.cpp
+++ Practice Code/Sorting Algorithm/Bubble Sort.cpp
@@ -34,3 +34,31 @@ int main()
 
 // Time complexity : O(n^2) - Average case & Worst Case, O(n) - Best Case -> When the array is already sorted..
 // Space Complexity : O(1) - One array is given , no need to extra space
+
+
+
+// Optimized version
+
+void bubbleSort(int arr[], int n){
+    int i, j, temp, flag=0;
+    
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n-i-1; j++){
+            
+            if( arr[j] > arr[j+1]){
+                // swap the elements
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                // if swapping happens update flag to 1
+                flag = 1;
+            } 
+        }
+        // if value of flag is zero after all the iterations of inner loop
+        // then break out
+        if(flag==0)
+        {
+            break;
+        }
+    }
+}
